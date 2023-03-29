8b5859cfc51d4fc08c0f5adb5fdf26d4d662c880
Chetan Kedia <chetankedia@yahoo.com>
2023-03-29 23:48:15 +0000

    commit

--- models/staging/sample_data/stg_sample_data__nations.sql
+++ models/staging/sample_data/stg_sample_data__nations.sql
@@ -10,9 +10,9 @@ transformed as (
 
   select 
     n_nationkey as nation_key,
-    n_regionkey as region_key
+    n_regionkey as region_key,
     -- add nation name here
-
+    n_name as nation_name
 
   from source
 
